9dfe15dbbb8ee1351d49ba5e584a0362749b21e1
edoko <r_data@naver.com>
2012-07-02 14:45:23 +0900

    Adjust MPU Clocks

--- arch/arm/mach-omap2/dpll44xx.c
+++ arch/arm/mach-omap2/dpll44xx.c
@@ -230,7 +230,7 @@ int omap4_prcm_freq_update(void)
 
 #define OMAP_1_5GHz	1500000000
 #define OMAP_1_2GHz	1200000000
-#define OMAP_1GHz	1050000000
+#define OMAP_1GHz	1000000000
 #define OMAP_920MHz	920000000
 #define OMAP_748MHz	748000000
 

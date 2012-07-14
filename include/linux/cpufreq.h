aa2514f383a77d53051a1dbd83754db38d1ccf79
edoko <r_data@naver.com>
2012-07-14 23:32:21 +0900

    cpufreq: Add SmartassV2 governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -376,6 +376,9 @@ extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SAKURACTIVE)
 extern struct cpufreq_governor cpufreq_gov_sakuractive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sakuractive)
+#elif defined(CPU_FREQ_DEFAULT_GOV_SMARTASS2)
+extern struct cpufreq_governor cpufreq_gov_smartass2;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartass2)
 #endif
 
 

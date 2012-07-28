a970707f10fc819706106e16be7ea1bb6c6a15b7
edoko <r_data@naver.com>
2012-07-28 21:33:05 +0900

    Update Linux 3.0.38

--- drivers/usb/serial/ftdi_sio_ids.h
+++ drivers/usb/serial/ftdi_sio_ids.h
@@ -794,7 +794,6 @@
 #define PI_VID              0x1a72  /* Vendor ID */
 #define PI_E861_PID         0x1008  /* E-861 piezo controller USB connection */
 
-
 /*
  * Bayer Ascensia Contour blood glucose meter USB-converter cable.
  * http://winglucofacts.com/cables/

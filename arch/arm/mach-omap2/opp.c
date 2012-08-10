4659cb3653946964fcd6288df8710b51b115b6b4
edoko <r_data@naver.com>
2012-08-11 00:50:55 +0900

    Revert "Update Linux 3.0.40"
    This reverts commit f52f2c005085b7d820da89111888e7866a05657b.

--- arch/arm/mach-omap2/opp.c
+++ arch/arm/mach-omap2/opp.c
@@ -58,7 +58,7 @@ int __init omap_init_opp_table(struct omap_opp_def *opp_def,
 	omap_table_init = 1;
 
 	/* Lets now register with OPP library */
-	for (i = 0; i < opp_def_size; i++, opp_def++) {
+	for (i = 0; i < opp_def_size; i++) {
 		struct omap_hwmod *oh;
 		struct device *dev;
 
@@ -112,11 +112,8 @@ int __init omap_init_opp_table(struct omap_opp_def *opp_def,
 				dev_err(dev, "%s:%s:err dvfs register %d %d\n",
 					__func__, opp_def->hwmod_name, r, i);
 		}
-<<<<<<< HEAD
 next:
 		opp_def++;
-=======
->>>>>>> ARM: OMAP2+: OPP: Fix to ensure check of right oppdef after bad one
 	}
 
 	return 0;
--- drivers/net/tun.c
+++ drivers/net/tun.c
@@ -1239,7 +1239,6 @@ static long __tun_chr_ioctl(struct file *file, unsigned int cmd,
 	int vnet_hdr_sz;
 	int ret;
 
-<<<<<<< HEAD
 #ifdef CONFIG_ANDROID_PARANOID_NETWORK
 	if (cmd != TUNGETIFF && !capable(CAP_NET_ADMIN)) {
 		return -EPERM;
@@ -1247,14 +1246,9 @@ static long __tun_chr_ioctl(struct file *file, unsigned int cmd,
 #endif
 
 	if (cmd == TUNSETIFF || _IOC_TYPE(cmd) == 0x89)
-=======
-	if (cmd == TUNSETIFF || _IOC_TYPE(cmd) == 0x89) {
->>>>>>> net/tun: fix ioctl() based info leaks
 		if (copy_from_user(&ifr, argp, ifreq_len))
 			return -EFAULT;
-	} else {
-		memset(&ifr, 0, sizeof(ifr));
-	}
+
 	if (cmd == TUNGETFEATURES) {
 		/* Currently this just means: "what IFF flags are valid?".
 		 * This is needed because we never checked for invalid flags on

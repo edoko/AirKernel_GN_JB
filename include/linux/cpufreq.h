6b01c02d78aceb70ccf6c7725659fbff9d5b84e0
edoko <r_data@naver.com>
2012-11-24 12:32:56 +0900

    Add PegasusQ governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -379,6 +379,9 @@ extern struct cpufreq_governor cpufreq_gov_sakuractive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDX)
 extern struct cpufreq_governor cpufreq_gov_ondemandx;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_ondemandx)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
+extern struct cpufreq_governor cpufreq_gov_pegasusq;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_pegasusq)
 #endif
 
 

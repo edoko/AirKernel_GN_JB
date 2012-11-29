c60865cfad0492782b2961ab0f6c7b02576918c0
edoko <r_data@naver.com>
2012-11-29 23:41:20 +0900

    Add 598Mhz clock and adjust OPP50 from 384Mhz to 364Mhz

--- arch/arm/mach-omap2/omap2plus-cpufreq.c
+++ arch/arm/mach-omap2/omap2plus-cpufreq.c
@@ -343,7 +343,7 @@ static int __cpuinit omap_cpu_init(struct cpufreq_policy *policy)
 
 	cpufreq_frequency_table_get_attr(freq_table, policy->cpu);
 
-	policy->min = 384000;
+	policy->min = 364000;
 	policy->max = 1228000;
 	policy->cur = omap_getspeed(policy->cpu);
 

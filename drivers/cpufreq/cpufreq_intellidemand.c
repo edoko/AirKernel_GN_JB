4b3b5be16743b648fffa2b41775672c8ac9d9a63
edoko <r_data@naver.com>
2012-12-04 00:26:34 +0900

    Adjust min clock from 236Mhz to 364Mhz

--- drivers/cpufreq/cpufreq_intellidemand.c
+++ drivers/cpufreq/cpufreq_intellidemand.c
@@ -55,7 +55,7 @@
 #define DBS_INPUT_EVENT_MIN_FREQ		(1200000)
 
 #ifdef CONFIG_CPUFREQ_ID_PERFLOCK
-#define DBS_PERFLOCK_MIN_FREQ			(236000)
+#define DBS_PERFLOCK_MIN_FREQ			(364000)
 #endif
 
 u64 freq_boosted_time;

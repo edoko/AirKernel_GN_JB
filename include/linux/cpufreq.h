7683c2a66398f83e1140ca5638b659f0850bbdea
Seongmin Park <r_data@naver.com>
2013-03-17 02:13:54 +0900

    Add ZZMOOVE governor (v0.3) -Credit to ZaneZam (Ported on Nexus 4 -by me) Signed-off-by: Seongmin Park <r_data@naver.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -408,6 +408,9 @@ extern struct cpufreq_governor cpufreq_gov_zenx;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAZY)
 extern struct cpufreq_governor cpufreq_gov_lazy;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_lazy)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
+extern struct cpufreq_governor cpufreq_gov_zzmoove;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_zzmoove)
 #endif
 
 
